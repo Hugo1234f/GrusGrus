7f28dbf56f29b4cf3c855f4c5c267c74443702d9
Hugo Friberg <friberghugo@gmail.com>
2025-05-10 22:49:20 +0200

    Fixed some issues.

--- premake5.lua
+++ premake5.lua
@@ -20,9 +20,38 @@ project "App"
     targetdir "bin/%{cfg.buildcfg}"
     staticruntime "off"
 
-    files { "src/**.h", "src/**.cpp" }
+    files { "src/App/**.h", "src/App/**.cpp" }
 
-    includedirs {"src"}
+    includedirs {"src/App"}
+
+    targetdir ("bin/" .. OutputDir)
+    objdir ("bin/ints/" .. OutputDir)
+
+    filter "system:windows"
+        systemversion "latest"
+        defines { "WINDOWS" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
+
+project "Stork"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    targetdir "bin/%{cfg.buildcfg}"
+    staticruntime "off"
+
+    files {"src/Stork/**.h", "src/Stork/**.cpp"}
+
+    includedirs { "src/Stork" }
 
     targetdir ("bin/" .. OutputDir)
     objdir ("bin/ints/" .. OutputDir)
---
+++ setup-win64-vs2022.bat
@@ -0,0 +1,6 @@
+premake5 vs2022
+
+mkdir src
+mkdir src\ints
+mkdir src\App
+mkdir src\Stork
