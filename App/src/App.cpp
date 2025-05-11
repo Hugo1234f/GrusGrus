32ceab2dd4ba4f8e615369dcd0b45c3a381447cb
Hugo Friberg <friberghugo@gmail.com>
2025-05-11 16:17:56 +0200

    Added mappings between ICAO codes and Aerodrome Names

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+
 #include "Stork.h"
+#include "App.h"
 
 int main() {
 	std::cout << "Hello App!" << std::endl;
---
+++ App/src/App.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+
+struct ICAOtoDromeNameStruct {
+	std::string ICAOCode;
+	std::string DroneName;
+} typedef ICAOtoDromeNameStruct;
+
+ICAOtoDromeNameStruct ICAOtoDromeName[] = {
+	{"ESCF", "Linköping/Malmen"},
+	{"ESCM", "Uppsala-Ärna"},
+	{"ESDF", "Ronneby Airport"},
+	{"ESGG", "Göteborg/Landvetter"},
+	{"ESGJ", "Jönköping Airport"},
+	{"ESGT", "Trollhättan/Vänersborg"},
+	{"ESGP", "Göteborg/Säve"},
+	{"ESIA", "Karlsborg"},
+	{"ESIB", "Såtenäs"},
+	{"ESKM", "Mora/Siljan"},
+	{"ESKN", "Stockholm/Skavsta"},
+	{"ESKS", "Sälen/Scandinavian Mountains"},
+	{"ESMK", "Kristianstad"},
+	{"ESMQ", "Kalmar"},
+	{"ESMS", "Malmö"},
+	{"ESMT", "Halmstad"},
+	{"ESMK", "Växjö/Kronoberg"},
+	{"ESND", "Sveg"},
+	{"ESNG", "Gällivare"},
+	{"ESNK", "Kramfors-Sollefteå"},
+	{"ESNL", "Lycksele"},
+	{"ESNN", "Sundsvall-Timrå"},
+	{"ESNO", "Örnsköldsvik"},
+	{"ESNQ", "Kiruna"},
+	{"ESNS", "Skellefteå"},
+	{"ESNU", "Umeå"},
+	{"ESNV", "Vilhelmina"},
+	{"ESNX", "Arvidsjaur"},
+	{"ESNZ", "Åre Östersund"},
+	{"ESOE", "Örebro"},
+	{"ESOK", "Karlstad"},
+	{"ESOH", "Hagfors"},
+	{"ESOW", "Stockholm/Västerås"},
+	{"ESPA", "Luleå/Kallax"},
+	{"ESPE", "Vidsel"},
+	{"ESSA", "Stockholm/Arlanda"},
+	{"ESSB", "Stockholm/Bromma"},
+	{"ESSD", "Borlänge"},
+	{"ESSL", "Linköping/SaaB"},
+	{"ESSP", "Norrköping/Kungsängen"},
+	{"ESST", "Torsby"},
+	{"ESSV", "Visby"},
+	{"ESTA", "Ängelholm"},
+	{"ESTL", "Ljungbyhed"},
+	{"ESUP", "Pajala"},
+	{"ESUT", "Hemavan Tärnaby"}
+}
+
+
+
