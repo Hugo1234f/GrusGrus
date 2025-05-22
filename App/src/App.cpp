9eb5cee58ef354815e8a7badebdc6b8b9e432ef9
Hugo Friberg <friberghugo@gmail.com>
2025-05-22 12:10:28 +0200

    Switched to Docking branch

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -34,7 +34,7 @@ int main() {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags | ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags | ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_ViewportsEnable;
 
 	ImGui::StyleColorsDark();
 
