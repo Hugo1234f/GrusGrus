6cfaba0548fed27454f3cbcd790b1c982a86deca
Hugo Friberg <friberghugo@gmail.com>
2025-07-06 00:01:54 +0200

    Swithed Widgets into its own class. Also made the refresh button work

---
+++ App/src/Widgets.cpp
@@ -0,0 +1,83 @@
+#include "Widgets.h"
+#include "Stork.h"
+#include "StorkGrabber.h"
+
+namespace widgets {
+
+		Stork::Grabber::MetarDataStruct metarData;
+		int aerodrome_highlited_idx = -1;
+		int aerodrome_item_selected_idx = 0;
+		std::string lastUpdated = "N/A";
+		static bool updateClicked = false;
+
+		static bool refresh = false;
+
+
+
+		Widgets::Widgets() {
+
+		}
+
+		void Widgets::setMetarData(Stork::Grabber::MetarDataStruct data) {
+			metarData = data;
+		}
+
+		void Widgets::drawLastUpdated() {
+			if (ImGui::CollapsingHeader("Last Updated", ImGuiTreeNodeFlags_None)) {
+				ImGui::Text(metarData.LastUpdated.c_str());
+	
+				if (ImGui::Button("Refresh")) {
+					updateClicked = true;
+				}
+				if (updateClicked) {
+					ImGui::SameLine();
+					ImGui::Text("Updating");
+					refresh = true;
+				}
+			}
+		}
+
+		void Widgets::drawStationsHeader() {
+			if (ImGui::CollapsingHeader("Stations", ImGuiBackendFlags_None)) {
+				int aerodrome_nums = ICAOtoDromeNameCount;
+
+				ImGui::SeparatorText("Aerodromes");
+				ImGui::BeginListBox("##Listbox 2", ImVec2(-FLT_MIN, (aerodrome_nums < 10 ? aerodrome_nums : 10) * ImGui::GetTextLineHeightWithSpacing()));
+				for (int i = 0; i < aerodrome_nums; i++) {
+					const bool is_selected = (aerodrome_item_selected_idx == i);
+					std::string ItemName = ICAOtoDromeName[i].ICAOCode + " - " + ICAOtoDromeName[i].DroneName;
+					ImGuiSelectableFlags flags = (aerodrome_highlited_idx == i) ? ImGuiSelectableFlags_Highlight : 0;
+
+					if (ImGui::Selectable(ItemName.c_str(), is_selected, flags)) {
+						aerodrome_item_selected_idx = i;
+					}
+				}
+				ImGui::EndListBox();
+
+				ImGui::SeparatorText("VViS");
+			}
+		}
+
+		void Widgets::drawSidePanel() {
+			ImGui::Begin("AD Data");
+
+			drawLastUpdated();
+			drawStationsHeader();
+
+			ImGui::End();
+		}
+
+		void Widgets::drawMap() {
+
+		}
+
+	
+		bool Widgets::getRefresh() {
+			return refresh;
+		}
+		void Widgets::setRefresh(bool state) {
+			refresh = state;
+			updateClicked = refresh;
+		}
+
+}
--- App/src/Widgets.h
+++ App/src/Widgets.h
@@ -2,58 +2,27 @@
 
 #include "imgui.h"
 #include "App.h"
+#include "StorkGrabber.h"
 
 namespace widgets {
+	
+	class Widgets 
+	{
 
-	static int aerodrome_highlited_idx = -1;
-	static int aerodrome_item_selected_idx = 0;
+	public:
+		Widgets();
+		
+		void setMetarData(Stork::Grabber::MetarDataStruct data);
+	
+		void drawLastUpdated();
+		void drawStationsHeader();
 
-	static std::string lastUpdated = "N/A";
+		//Draws the side panel
+		void drawSidePanel();
 
-	void setLastUpdated(std::string lastUpdatedStr) {
-		lastUpdated = lastUpdatedStr;
-	}
+		void drawMap();
 
-	void drawLastUpdated() {
-		if (ImGui::CollapsingHeader("Last Updated", ImGuiTreeNodeFlags_None)) {
-			ImGui::Text(lastUpdated.c_str());
-			ImGui::Button("Refresh (TODO)");
-		}
-	}
-
-	void drawStationsHeader() {
-		if (ImGui::CollapsingHeader("Stations", ImGuiBackendFlags_None)) {
-			int aerodrome_nums = sizeof(ICAOtoDromeName) / sizeof(ICAOtoDromeNameStruct);
-
-			ImGui::SeparatorText("Aerodromes");
-			ImGui::BeginListBox("##Listbox 2", ImVec2(-FLT_MIN, (aerodrome_nums < 10 ? aerodrome_nums : 10) * ImGui::GetTextLineHeightWithSpacing()));
-			for (int i = 0; i < aerodrome_nums; i++) {
-				const bool is_selected = (aerodrome_item_selected_idx == i);
-				std::string ItemName = ICAOtoDromeName[i].ICAOCode + " - " + ICAOtoDromeName[i].DroneName;
-				ImGuiSelectableFlags flags = (aerodrome_highlited_idx == i) ? ImGuiSelectableFlags_Highlight : 0;
-
-				if (ImGui::Selectable(ItemName.c_str(), is_selected, flags)) {
-					aerodrome_item_selected_idx = i;
-				}
-			}
-			ImGui::EndListBox();
-
-			ImGui::SeparatorText("VViS");
-		}
-	}
-
-	//Draws the side panel
-	void drawSidePanel() {
-		ImGui::Begin("AD Data");
-
-		drawLastUpdated();
-		drawStationsHeader();
-
-		ImGui::End();
-	}
-
-
-	void drawMap() {
-
-	}
+		bool getRefresh();
+		void setRefresh(bool state);
+	};
 }
