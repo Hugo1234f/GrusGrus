d0438184831a4ffd519f44dc017cc4abc49b9647
Hugo Friberg <friberghugo@gmail.com>
2025-07-08 16:41:42 +0200

    Added functionality to draw custom images

--- App/src/Renderer.cpp
+++ App/src/Renderer.cpp
@@ -1,6 +1,9 @@
 #include "Renderer.h"
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "misc/stb_image.h"
+
 namespace Renderer {
 
 	//------------------------------
@@ -72,6 +75,8 @@ namespace Renderer {
 				*quit = true;
 			}
 		}
+
+		return 0;
 	}
 
 	//------------------------------
@@ -81,6 +86,8 @@ namespace Renderer {
 		ImGui_ImplSDLRenderer3_NewFrame();
 		ImGui_ImplSDL3_NewFrame();
 		ImGui::NewFrame();
+
+		return 0;
 	}
 
 	//------------------------------
@@ -107,6 +114,62 @@ namespace Renderer {
 		SDL_DestroyRenderer(*renderer);
 		SDL_DestroyWindow(*window);
 		SDL_Quit();
+
+		return 0;
+	}
+
+	//------------------------------
+	// LoadTextureFromMemory
+	//------------------------------
+	int SDL3_LoadTextureFromMemory(const void* data, size_t data_size, SDL_Renderer* renderer, SDL_Texture** out_texture, int* out_width, int* out_height) {
+		int image_width = 0;
+		int image_height = 0;
+		int channels = 4;
+		unsigned char* image_data = stbi_load_from_memory((const unsigned char*)data, (int)data_size, &image_width, &image_height, NULL, 4);
+		if (image_data == nullptr)
+		{
+			fprintf(stderr, "Failed to load image: %s\n", stbi_failure_reason());
+			return 1;
+		}
+
+		SDL_Surface* surface = SDL_CreateSurfaceFrom(image_width, image_height, SDL_PIXELFORMAT_RGBA32, (void*)image_data, channels * image_width);
+		if (surface == nullptr)
+		{
+			fprintf(stderr, "Failed to create SDL surface: %s\n", SDL_GetError());
+			return 2;
+		}
+
+		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+		if (texture == nullptr)
+			fprintf(stderr, "Failed to create SDL texture: %s\n", SDL_GetError());
+
+		*out_texture = texture;
+		*out_width = image_width;
+		*out_height = image_height;
+
+		SDL_DestroySurface(surface);
+		stbi_image_free(image_data);
+
+		return 0;
+	}
+
+	// Open and read a file, then forward to LoadTextureFromMemory()
+	int SDL3_LoadTextureFromFile(const char* file_name, SDL_Renderer* renderer, SDL_Texture** out_texture, int* out_width, int* out_height)
+	{
+		FILE* f = fopen(file_name, "rb");
+		if (f == NULL)
+			return false;
+		fseek(f, 0, SEEK_END);
+		size_t file_size = (size_t)ftell(f);
+		if (file_size == -1)
+			return false;
+		fseek(f, 0, SEEK_SET);
+		void* file_data = IM_ALLOC(file_size);
+		fread(file_data, 1, file_size, f);
+		fclose(f);
+		bool ret = !SDL3_LoadTextureFromMemory(file_data, file_size, renderer, out_texture, out_width, out_height);
+		IM_FREE(file_data);
+		return ret;
 	}
 
 }
--- App/src/Renderer.h
+++ App/src/Renderer.h
@@ -10,6 +10,7 @@
 #define WINDOW_HEIGHT	1080
 #define WINDOW_TITLE	"GrusGrus App"
 
+
 namespace Renderer {
 
 	//------------------------------
@@ -40,5 +41,8 @@ namespace Renderer {
 	int ImGui_SDL3_ShutDown(SDL_Window** window, SDL_Renderer**	renderer);
 
 	//------------------------------
+
+	int SDL3_LoadTextureFromMemory(const void* data, size_t data_size, SDL_Renderer* renderer, SDL_Texture** out_texture, int* width, int* height);
+	int SDL3_LoadTextureFromFile(const char* file_name, SDL_Renderer* renderer, SDL_Texture** out_texture, int* out_width, int* out_height);
 }
 
